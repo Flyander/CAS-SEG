3856e75e1ab2644600880f2cf05e9ec68cd2525a
pierre gelin <ulan115@hotmail.fr>
2019-11-28 11:40:14 +0100

    creation classe commune

--- CAS SEG/Modeles/Commune.cs
+++ CAS SEG/Modeles/Commune.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,53 @@ namespace CAS_SEG.Modeles
 {
     class Commune
     {
+        #region Attributs
+        public static ArrayList collClassCommune = new ArrayList();
+
+        private int _numCom;
+        private string _nomCom;
+
+
+        #endregion
+
+        #region constructeur
+        public Commune(int numCom, string nomCom)
+        {
+            NumCom = numCom;
+            NomCom = nomCom;
+        }
+        #endregion
+
+        #region getSet
+        public int NumCom { get => _numCom; set => _numCom = value; }
+        public string NomCom { get => _nomCom; set => _nomCom = value; }
+        #endregion
+
+        #region methodes
+        public void ajouterUnSecteur()
+        {
+
+        }
+
+        public ArrayList secteurEV()
+        {
+            return collClassCommune;
+        }
+
+        public int volumeVannes()
+        {
+            return 0;
+        }
+
+        public string pertes()
+        {
+            return "a";
+        }
+
+        public string anomalies()
+        {
+            return "a";
+        }
+        #endregion
     }
 }
